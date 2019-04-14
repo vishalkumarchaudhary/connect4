1ee2c4085b2b9f04b728b56a74e7dd3cd4a0cb8c
Vishalkumarchaudhary <111501030@smail.iitpkd.ac.in>
2019-04-15 01:02:31 +0530

    tournament : MCTS Problem 1.5

--- connect4/tensorflow/NNet.py
+++ connect4/tensorflow/NNet.py
@@ -112,7 +112,8 @@ class NNetWrapper(NeuralNet):
 
         # print('PREDICTION TIME TAKEN : {0:03f}'.format(time.time()-start))
         runs, _ = self.game.getReward(self.game.wicket_in_hand-9,np.argmax(bowler), np.argmax(shots))
-
+        if (np.sum(bowler) == 0):
+            print("batting probab ", bowler[0], "State is ", state)
         # return (shots.T * bowler).flatten(), runs
         return shots[0], runs
 
